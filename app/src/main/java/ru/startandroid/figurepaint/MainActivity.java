87777e4deb3da448166c580b00486e90b4907769
mouvingame <mouvingame@gmail.com>
2016-11-20 20:19:49 +0300

    add animation for imageButtons

--- app/src/main/java/ru/startandroid/figurepaint/MainActivity.java
+++ app/src/main/java/ru/startandroid/figurepaint/MainActivity.java
@@ -1,13 +1,8 @@
 package ru.startandroid.figurepaint;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,8 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        Toolbar tb;
 
     }
 }
